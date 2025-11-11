d0b81cec4f8d17c27bfd741da138760a352e4f2a
gerardohuaman <U202313990@upc.edu.pe>
2025-11-10 23:06:00 -0500

    adding frontend files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Inicio } from './components/inicio/inicio';
+import { Conocer } from './components/conocer/conocer';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Inicio, Conocer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/conocer/conocer.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-conocer',
+  imports: [RouterLink],
+  templateUrl: './conocer.html',
+  styleUrls: ['./conocer.css'],
+})
+export class Conocer {
+
+}
