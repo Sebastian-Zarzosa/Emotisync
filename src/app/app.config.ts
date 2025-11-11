3cdbf8990449e0d110bd59e945512be297b45bf8
gerardohuaman <u202313990@upc.edu.pe>
2025-11-11 09:46:08 -0500

    appconfig modified

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,9 @@
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection,
-} from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideBrowserGlobalErrorListeners } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';;
 
 export const appConfig: ApplicationConfig = {
   providers: [
