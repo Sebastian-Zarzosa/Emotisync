44c13bd4cdb632b989686f8a52cdf74345392eee
gerardohuaman <u202313990@upc.edu.pe>
2025-11-24 21:21:32 -0500

    deleting duplicate Environment

--- src/app/services/ejerciciosservice.ts
+++ src/app/services/ejerciciosservice.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../environments/environment';
+import { environment } from '../../environments/environment';
 import { Ejercicio } from '../models/ejercicio';
 
 const base_url= environment.base;
