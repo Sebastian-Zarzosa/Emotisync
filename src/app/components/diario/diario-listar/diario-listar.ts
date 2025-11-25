436fb54e2bff3fdd9a113b9e75c7f508d2371fc8
Sebastian <sebas_21_zc@outlook.es>
2025-11-24 19:48:27 -0500

    add diarios to panel

--- src/app/components/diario/diario-listar/diario-listar.ts
+++ src/app/components/diario/diario-listar/diario-listar.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { Diario } from '../../../models/Diario';
 import { Diarioservice } from '../../../services/diarioservice';
@@ -21,7 +21,7 @@ import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
   templateUrl: './diario-listar.html',
   styleUrl: './diario-listar.css',
 })
-export class DiarioListar implements OnInit {
+export class DiarioListar implements OnInit, AfterViewInit {
   dataSource: MatTableDataSource<Diario> = new MatTableDataSource();
   displayedColumns: string[] = [
     'fecha',
@@ -53,5 +53,9 @@ export class DiarioListar implements OnInit {
         this.diarioService.setLista(data);
       });
     });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 }
