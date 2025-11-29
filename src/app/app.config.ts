9c3480011f70d0cb2501f978fbc30f0e7090a837
Sebastian <sebas_21_zc@outlook.es>
2025-11-28 20:59:36 -0500

    deploy for firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,19 +6,21 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptors,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { provideNativeDateAdapter } from '@angular/material/core';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { JwtModule } from '@auth0/angular-jwt';
 import { loadingInterceptor } from './core/interceptors/loading.interceptor';
 
-export function tokenGetter(){
+export function tokenGetter() {
   return sessionStorage.getItem('token');
 }
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideAnimationsAsync(),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
@@ -29,12 +31,12 @@ export const appConfig: ApplicationConfig = {
     provideNativeDateAdapter(),
     importProvidersFrom(
       JwtModule.forRoot({
-        config:{
+        config: {
           tokenGetter: tokenGetter,
           allowedDomains: ['localhost:8080'],
-          disallowedRoutes: ['http://localhost:8080/login/forget']
-        }
+          disallowedRoutes: ['http://localhost:8080/login/forget'],
+        },
       })
-    )
+    ),
   ],
 };
